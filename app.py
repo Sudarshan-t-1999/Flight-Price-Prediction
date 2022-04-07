a42e7d4ebf0004198206f27b722d4f8f9e516050
Sudarshan T <sudarshant1999@outlook.com>
2022-04-07 20:43:14 +0530

    Adding comments

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+# importing required libraries
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
+# creating the app
 app = Flask(__name__)
 
-
+# rendering the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+# rendering the result page
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     if request.method == 'POST':
+        # accepting the inputs from the webpage and storing them inside a dictionary to be converted into a dataframe 
         data = {
                 'Airline':[request.form['Airline']],
                 'Source':[request.form['Source']],
@@ -27,6 +30,8 @@ def test():
                 'Departure Hour':[int(request.form['dep_time'].split(':')[0])],
                 'Departure Minute':[int(request.form['dep_time'].split(':')[1])]
                 }
+        
+        # calculating the duration of the flight in terms of minutes. 
         day_of_arrival= int(request.form['Date_of_Arrival'].split('-')[-1])
         departure_hour= int(data["Departure Hour"][0])
         arrival_hour= int(data['Arrival Hour'][0])
@@ -36,25 +41,35 @@ def test():
     
         dur = (((24-departure_hour) + ((day_of_arrival-day_of_departure-1)*24) + arrival_hour)*60) - deparure_minute + arrival_minute
 
+        # adding the duration to the dictionary
         data.update({'Duration Min': dur})
+        
+        # converting the dictionary into a dataframe
         input_df = pd.DataFrame(data)
         
+        # loading the joblib file
         file = joblib.load('flight price prediction model.joblib')
-
+        
+        # extracting the necessary elements to perform data preprocessing
         model = file['model']
         scaler=file['scaler']
         numerical_cols=file['numerical columns']
         categorical_cols= file['categorical columns']
         encoders= file.get('encoders')
 
+        # performing label encoding to all the categorical columns
         for col in categorical_cols:
             input_df[col] = encoders[col].transform(input_df[col])
-
+        
+        # performing data preprocessing
         input_df = scaler.transform(input_df)
         input_df = pd.DataFrame(input_df)
         input_df.columns = data.keys()
+        
+        # using the model to predict the flight ticket price
         prediction = model.predict(input_df)
-
+        
+        # rendering the webpage to show the predicted answer
         return render_template('test.html', prediction=float(prediction[0]))
 
 
